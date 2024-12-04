f9c758b261907bf67f236f61c9d25586e9015930
Deimek <333olegovich333@gmail.com>
2024-12-04 16:43:14 +0100

    chenges

--- script.js
+++ script.js
@@ -1,28 +1,29 @@
-const stringOne = prompt('enter your information', 'hello');
+// const
+let stringOne = prompt('enter your information', 'hello');
 if (stringOne === ' ') {
-    stringOne = prompt('enter your information in the correct', 'i am string');
+    stringOne = prompt('enter your information in the correct', 'hello');
 } else if (stringOne === null) {
-    stringOne = prompt('enter your information', 'i am string');
+    stringOne = prompt('enter your informati, hello');
 } else if (stringOne.length < 2) {
-    stringOne = prompt('enter your information in the correct', 'i am string');
+    stringOne = prompt('enter your information in the correct', 'hello');
 }
-
-const stringTwo = prompt('enter your information', 'hello');
+// const
+let stringTwo = prompt('enter your information', 'hello');
 if (stringTwo === ' ') {
-    stringTwo = prompt('enter your information in correct fofmat', 'i am string');
+    stringTwo = prompt('enter your information in correct fofmat', 'hello');
 } else if (stringTwo === null) {
-    stringTwo = prompt('enter your information', 'i am string');
+    stringTwo = prompt('enter your information', 'hello');
 } else if (stringTwo.length < 2) {
-    stringTwo = prompt('enter your information in the correct', 'i am string');
+    stringTwo = prompt('enter your information in the correct', 'hello');
 }
-
-const stringThree = prompt('enter your information', 'hello');
+// const
+let stringThree = prompt('enter your information', 'hello');
 if (stringThree === ' ') {
-    stringThree = prompt('enter your information in the correct fofmat', 'i am string');
+    stringThree = prompt('enter your information in the correct fofmat', 'hello');
 } else if (stringThree === null) {
-    stringThree = prompt('enter your information', 'i am string');
+    stringThree = prompt('enter your information', 'hello');
 } else if (stringThree.length < 2) {
-    stringThree = prompt('enter your information in the correct', 'i am string');
+    stringThree = prompt('enter your information in the correct', 'hello');
 }
 
 let strings = `${stringOne}, ${stringTwo}, ${stringThree}`;
