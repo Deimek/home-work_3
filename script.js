25927a390a09b1d7c1974389f1c123876b65d07d
Deimek <333olegovich333@gmail.com>
2024-12-07 15:58:54 +0100

    modified

--- script.js
+++ script.js
@@ -1,31 +1,45 @@
 // const
 let stringOne = prompt('enter your information', 'hello');
-if (stringOne === ' ') {
-    stringOne = prompt('enter your information in the correct', 'hello');
-} else if (stringOne === null) {
-    stringOne = prompt('enter your informati, hello');
+
+if (stringOne === null) {
+    stringOne = prompt('enter your information or goodbay', 'hello');
+    if (stringOne === null) {
+        alert('Good bay');
+        stringOne = 'see you';
+    }
+} else if (!stringOne.trim()) {
+    stringOne = prompt('enter your information, hello');
 } else if (stringOne.length < 2) {
     stringOne = prompt('enter your information in the correct', 'hello');
 }
 // const
 let stringTwo = prompt('enter your information', 'hello');
-if (stringTwo === ' ') {
-    stringTwo = prompt('enter your information in correct fofmat', 'hello');
-} else if (stringTwo === null) {
+if (stringTwo === null) {
+    stringTwo = prompt('enter your information or goodbay', 'hello');
+    if (stringTwo === null) {
+        alert('Good bay');
+        stringTwo = 'see you';
+    }
+} else if (!stringTwo.trim()) {
     stringTwo = prompt('enter your information', 'hello');
 } else if (stringTwo.length < 2) {
     stringTwo = prompt('enter your information in the correct', 'hello');
 }
 // const
 let stringThree = prompt('enter your information', 'hello');
-if (stringThree === ' ') {
-    stringThree = prompt('enter your information in the correct fofmat', 'hello');
-} else if (stringThree === null) {
+if (stringThree === null) {
+    stringThree = prompt('enter your information or goodbay', 'hello');
+    if (stringThree === null) {
+        alert('Good bay');
+        stringThree = 'see you';
+    }
+} else if (!stringThree.trim()) {
     stringThree = prompt('enter your information', 'hello');
 } else if (stringThree.length < 2) {
     stringThree = prompt('enter your information in the correct', 'hello');
 }
 
-let strings = `${stringOne}, ${stringTwo}, ${stringThree}`;
+const strings = `${stringOne}, ${stringTwo}, ${stringThree}`;
 
 console.log(strings);
+alert(strings);
